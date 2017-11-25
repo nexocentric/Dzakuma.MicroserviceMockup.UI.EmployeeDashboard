75e3a0729c405cc43565700a1a0441dd066bc5d1
Dodzidenu Y. Dzakuma <nexocentric@yahoo.com>
2017-11-25 16:59:05 -0600

    Finished a number of changes for the demo UI.
    I just need to refactor and test. Then I need to implement the movie display window in a different program.

--- Dzakuma.MicroserviceMockup.UI.EmployeeDashboard/Dashboard.xaml.cs
+++ Dzakuma.MicroserviceMockup.UI.EmployeeDashboard/Dashboard.xaml.cs
@@ -28,6 +28,9 @@ namespace Dzakuma.MicroserviceMockup.UI.EmployeeDashboard
 		{
 			InitializeComponent();
 			RefreshPersonnelList();
+			GetBioInfoormation("1");
+			LoadBioPicture("1", "Male");
+			LoadAnimalPreference(0);
 		}
 
 		private void GeneralEmployeeDataRefresh_OnClick(object sender, RoutedEventArgs e)
@@ -53,6 +56,59 @@ namespace Dzakuma.MicroserviceMockup.UI.EmployeeDashboard
 			}
 
 			return PersonnelList.Items.Count;
+		}
+
+		private void LoadBioPicture(string id, string gender)
+		{
+			string genderId = gender == "Female" ? "woman" : "man";
+			var picture = new BitmapImage();
+			picture.BeginInit();
+			picture.UriSource = new Uri($@"https://loremflickr.com/300/300/portrait,{genderId}?lock={id}", UriKind.Absolute);
+			picture.EndInit();
+			Mug.Source = picture;
+		}
+
+		public void GetBioInfoormation(string id)
+		{
+			var data = _dataSiphon.DeserializeJsonString(_executablePath, $"--general --id {id}");
+			var employeeData = data.individualData;
+
+			EmployeeName.Text = string.Format(
+				"Employee Name: {0} {1}",
+				(string)employeeData["first_name"],
+				(string)employeeData["last_name"]
+			);
+
+			EmployeeDepartment.Text = string.Format(
+				"Department: {0}",
+				(string)employeeData["department"]
+			);
+		}
+
+		private void LoadAnimalPreference(int preference)
+		{
+			string imageUrl = preference == 0 ? @"https://loremflickr.com/200/200/cat" : @"https://placebear.com/200/200";
+			var picture = new BitmapImage();
+			picture.BeginInit();
+			picture.UriSource = new Uri(imageUrl, UriKind.Absolute);
+			picture.EndInit();
+			AnimalPreference.Source = picture;
+		}
+
+		private void PersonelList_OnMouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+		{
+			try
+			{
+				var selectedItem = (WrapPanel)sender;
+				var id = (string)selectedItem.Uid;
+				GetBioInfoormation(id);
+				LoadBioPicture(id, "Male"); //this is a part of the demo that needs to be changed
+				LoadAnimalPreference(0);
+			}
+			catch (Exception anomaly)
+			{
+				
+			}
 		}
 	}
 }
