53f15b256f66949ffe24dd5003204b53c86e96d4
Dodzidenu Y. Dzakuma <nexocentric@yahoo.com>
2018-01-15 13:24:29 -0600

    Added a testing UI for WPF.

---
+++ Dzakuma.MicroserviceMockup.UI.EmployeeDashboardTests/PersonnelInformationSelectorUITests.cs
@@ -0,0 +1,34 @@
+﻿using System.Linq;
+using System.Windows.Controls;
+using Dzakuma.MicroserviceMockup.UI.EmployeeDashboard;
+using Xunit;
+using FlaUI;
+using FlaUI.Core;
+using FlaUI.UIA3;
+
+namespace Dzakuma.MicroserviceMockup.UI.EmployeeDashboardTests
+{
+	public class PersonnelInformationSelectorUITests
+	{
+		[Fact]
+		public void InitialUITest()
+		{
+			var applicationPath = @".\Dzakuma.MicroserviceMockup.UI.EmployeeDashboard.exe";
+			var application = Application.Launch(applicationPath);
+
+			using (var automationManager = new UIA3Automation())
+			{
+				var window = application.GetMainWindow(automationManager);
+				var employeeList = window.FindFirstDescendant(cf => cf.ByAutomationId("PersonnelList"));
+
+				var employeeNameOne = window.FindFirstDescendant(cf => cf.ByAutomationId("EmployeeName")).Name;
+				employeeList.FindChildAt(10)?.AsListBoxItem().FindFirstChild().AsButton()?.Invoke();
+				var employeeNameTwo = window.FindFirstDescendant(cf => cf.ByAutomationId("EmployeeName")).Name;
+
+				Assert.NotEqual(employeeNameOne, employeeNameTwo);
+			}
+
+			application.Close();
+		}
+	}
+}
